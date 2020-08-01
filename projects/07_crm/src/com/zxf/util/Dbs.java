5d637d7064055fd0e91a4700eb77b5958f80bb20
飞熊 <310311589@qq.com>
2020-08-02 02:03:48 +0800

    spring-jdbc, beanutils 使用 crm项目增删改查的实现

---
+++ projects/07_crm/src/com/zxf/util/Dbs.java
@@ -0,0 +1,34 @@
+package com.zxf.util;
+
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class Dbs {
+
+    private static JdbcTemplate tpl;
+
+    static {
+        try (InputStream is = Dbs.class.getClassLoader().getResourceAsStream("druid.properties")) {
+            Properties properties = new Properties();
+            properties.load(is);
+            DataSource ds = DruidDataSourceFactory.createDataSource(properties);
+            tpl = new JdbcTemplate(ds);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static JdbcTemplate getTpl() {
+        return tpl;
+    }
+}
